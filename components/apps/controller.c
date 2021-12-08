42a62ce68227cc626926b963f5862daef40c2f1b
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2021-12-08 10:27:04 +0800

    Reorganize controller.c

--- components/apps/controller.c
+++ components/apps/controller.c
@@ -34,47 +34,47 @@ typedef struct command_reg_t {
 } command_reg_t;
 
 static command_reg_t s_registration[] = {
-    {.name = "restart", .func=command_func_restart},
-    {.name = "ping", .func=command_func_ping},
-    {.name = "shutdown", .func=command_func_shutdown},
-    {.name = "update", .func=command_func_update},
-    {.name = "imu_cali_reset", .func=command_func_imu_cali_reset},
-    {.name = "imu_cali_acc", .func=command_func_imu_cali_acc},
-    {.name = "imu_cali_mag", .func=command_func_imu_cali_mag},
-    {.name = "start", .func=command_func_start},
-    {.name = "stop", .func=command_func_stop},
-    {.name = "imu_enable", .func=command_func_imu_enable},
-    {.name = "imu_disable", .func=command_func_imu_disable},
-    {.name = "imu_status", .func=command_func_imu_status},
-    {.name = "imu_imm", .func=command_func_imu_imm},
-    {.name = "imu_setup", .func=command_func_imu_setup},
-    {.name = "imu_scale",.func=command_func_imu_scale},
-    {.name = "id", .func=command_func_id},
-    {.name = "ver", .func=command_func_ver},
-    {.name = "blink_set", .func=command_func_blink_set},
-    {.name = "blink_get", .func=command_func_blink_get},
-    {.name = "blink_start", .func=command_func_blink_start},
-    {.name = "blink_stop", .func=command_func_blink_stop},
-    {.name = "blink_off",.func=command_func_blink_off},
-    {.name = "self_test", .func=command_func_self_test},
-    {.name = "always_on", .func=command_func_always_on},
-    {.name = "v"CONFIG_FIRMWARE_VERSION"_shutdown", .func=command_func_shutdown}
+    {.name = "restart", .func = command_func_restart},
+    {.name = "ping", .func = command_func_ping},
+    {.name = "shutdown", .func = command_func_shutdown},
+    {.name = "update", .func = command_func_update},
+    {.name = "imu_cali_reset", .func = command_func_imu_cali_reset},
+    {.name = "imu_cali_acc", .func = command_func_imu_cali_acc},
+    {.name = "imu_cali_mag", .func = command_func_imu_cali_mag},
+    {.name = "start", .func = command_func_start},
+    {.name = "stop", .func = command_func_stop},
+    {.name = "imu_enable", .func = command_func_imu_enable},
+    {.name = "imu_disable", .func = command_func_imu_disable},
+    {.name = "imu_status", .func = command_func_imu_status},
+    {.name = "imu_imm", .func = command_func_imu_imm},
+    {.name = "imu_setup", .func = command_func_imu_setup},
+    {.name = "imu_scale",.func = command_func_imu_scale},
+    {.name = "id", .func = command_func_id},
+    {.name = "ver", .func = command_func_ver},
+    {.name = "blink_set", .func = command_func_blink_set},
+    {.name = "blink_get", .func = command_func_blink_get},
+    {.name = "blink_start", .func = command_func_blink_start},
+    {.name = "blink_stop", .func = command_func_blink_stop},
+    {.name = "blink_off",.func = command_func_blink_off},
+    {.name = "self_test", .func = command_func_self_test},
+    {.name = "always_on", .func = command_func_always_on},
+    {.name = "v"CONFIG_FIRMWARE_VERSION"_shutdown", .func = command_func_shutdown}
 };
 
 #define MATCH_CMD(x, cmd) (strncasecmp(x, cmd, strlen(cmd)) == 0)
 
-command_reg_t * parse_command(char* command, int len) {
+command_reg_t* parse_command(char* command, int len) {
     ESP_LOGI(TAG, "Got command %s from controller", command);
-   for (int idx = 0; idx < sizeof(s_registration) / sizeof(command_reg_t); ++idx) {
-       if (MATCH_CMD(command, s_registration[idx].name)) {
-           return &s_registration[idx];
-       }
-   }
-   return NULL;
+    for (int idx = 0; idx < sizeof(s_registration) / sizeof(command_reg_t); ++idx) {
+        if (MATCH_CMD(command, s_registration[idx].name)) {
+            return &s_registration[idx];
+        }
+    }
+    return NULL;
 }
 
 esp_err_t execute_command(char* rx_buffer, char* tx_buffer, size_t rx_len, size_t tx_len) {
-    command_reg_t * cmd = parse_command(rx_buffer, rx_len); // TODO: Migrate another command parser
+    command_reg_t* cmd = parse_command(rx_buffer, rx_len); // TODO: Migrate another command parser
     esp_err_t ret = ESP_FAIL;
     bzero(tx_buffer, sizeof(char) * tx_len);
 
@@ -92,7 +92,7 @@ esp_err_t execute_command(char* rx_buffer, char* tx_buffer, size_t rx_len, size_
 
         /** Fill tx_buffer with '\0' **/
         /** Fill tx buffer with command related context **/
-        ret = cmd->func(rx_buffer, rx_len, tx_buffer, tx_len);
+        ret = cmd->func(rx_buffer, rx_len, tx_buffer, tx_len);// TODO: add cmd to g_imu
 
         /** Command did not modify the buffer **/
         if (strlen(tx_buffer) == 0) {
@@ -104,70 +104,72 @@ esp_err_t execute_command(char* rx_buffer, char* tx_buffer, size_t rx_len, size_
     }
 };
 
-static void interact(const int sock)
-{
-    int len;
+#define send_all(sock, buf) \
+        { \
+            int to_write = strlen(buf); \
+            while (to_write > 0) { \
+                int written = send(sock, buf, to_write, 0); \
+                if (written < 0) { \
+                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno); \
+                    break; \
+                } \
+                to_write -= written; \
+            } \
+        }
+
+static void interact(const int sock) {
+    int recv_len;
     ESP_LOGI(TAG, "Interacting");
     do {
-        len = recv(sock, s_rx_buffer, sizeof(s_rx_buffer) - 1, 0);
-        if (len < 0) {
+        recv_len = recv(sock, s_rx_buffer, sizeof(s_rx_buffer) - 1, 0);
+        if (recv_len < 0) {
             ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
-        } else if (len == 0) {
+        } else if (recv_len == 0) {
             ESP_LOGW(TAG, "Connection closed");
         } else {
-            s_rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
-            ESP_LOGI(TAG, "Received %d bytes: %s", len, s_rx_buffer);
+            s_rx_buffer[recv_len] = 0; // Null-terminate whatever is received and treat it like a string
+            ESP_LOGI(TAG, "Received %d bytes: %s", recv_len, s_rx_buffer);
 
             device_reset_sleep_countup();
             execute_command(s_rx_buffer, s_tx_buffer, RX_BUFFER_LEN, TX_BUFFER_LEN);
 
-            int to_write = strlen(s_tx_buffer);
-            while (to_write > 0) {
-                int written = send(sock, s_tx_buffer, to_write, 0);
-                if (written < 0) {
-                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-                    break;
-                }
-                to_write -= written;
-            }
+            send_all(sock, s_tx_buffer);
         }
-    } while (len > 0);
+    } while (recv_len > 0);
 }
+
+#define CONFIG_KEEP_ALIVE 1
+#define CONFIG_KEEP_IDLE 5
+#define CONFIG_KEEP_COUNT 3
+#define CONFIG_KEEP_INTERVAL 5
 
 void app_controller(void* pvParameters) {
     ESP_LOGI(TAG, "app_controller started");
 
-    // char s_addr_str[128];
     int addr_family = 0;
     int ip_protocol = 0;
-
-    int KEEP_ALIVE = 1;
-    int KEEP_IDLE = 5;
-    int KEEP_COUNT = 3;
-    int KEEP_INTERVAL = 5;
 
     struct timeval timeout = { 0, 0 }; // UDP timeout
 
     while (1) {
-
         /** Create address struct **/
         struct sockaddr_in dest_addr;
         dest_addr.sin_addr.s_addr = htonl(INADDR_ANY);
         dest_addr.sin_family = AF_INET;
         dest_addr.sin_port = htons(CONFIG_LOCAL_PORT);
-        addr_family = AF_INET;
-        ip_protocol = IPPROTO_IP;
 
         /** Create STREAM socket **/
-        int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
-        if (listen_sock < 0) {
+        addr_family = AF_INET;
+        ip_protocol = IPPROTO_IP;
+        int server_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+        if (server_sock < 0) {
             ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            device_delay_ms(1000);
             goto socket_error;
         }
 
         /** Bind socket **/
-        int err = bind(listen_sock, (struct sockaddr*)&dest_addr, sizeof(dest_addr));
+        int err = bind(server_sock, (struct sockaddr*)&dest_addr, sizeof(dest_addr));
         if (err < 0) {
             ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
             goto socket_error;
@@ -175,34 +177,39 @@ void app_controller(void* pvParameters) {
         ESP_LOGI(TAG, "Socket bound, port %d", CONFIG_LOCAL_PORT);
 
         /** Start listenning **/
-        err = listen(listen_sock, 1);
+        err = listen(server_sock, 1);
         if (err != 0) {
             ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
             goto socket_error;
         }
 
-        /** Set socket options **/
+        /** Set listen socket options **/
         int opt = 1;
-        setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-        setsockopt(listen_sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
+        setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+        setsockopt(server_sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
 
         struct sockaddr_storage source_addr;
         socklen_t socklen = sizeof(source_addr);
 
         while (1) {
-            ESP_LOGI(TAG, "Socket listening");
+            int KEEP_ALIVE = CONFIG_KEEP_ALIVE;
+            int KEEP_IDLE = CONFIG_KEEP_IDLE;
+            int KEEP_COUNT = CONFIG_KEEP_COUNT;
+            int KEEP_INTERVAL = CONFIG_KEEP_INTERVAL;
 
-            int sock = accept(listen_sock, (struct sockaddr*)&source_addr, &socklen);
-            if (sock < 0) {
+            ESP_LOGD(TAG, "Socket listening");
+
+            int client_sock = accept(server_sock, (struct sockaddr*)&source_addr, &socklen);
+            if (client_sock < 0) {
                 ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
                 break;
             }
 
             /** Set tcp keepalive option **/
-            setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &KEEP_ALIVE, sizeof(int));
-            setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &KEEP_IDLE, sizeof(int));
-            setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &KEEP_INTERVAL, sizeof(int));
-            setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &KEEP_COUNT, sizeof(int));
+            setsockopt(client_sock, SOL_SOCKET, SO_KEEPALIVE, &KEEP_ALIVE, sizeof(int));
+            setsockopt(client_sock, IPPROTO_TCP, TCP_KEEPIDLE, &KEEP_IDLE, sizeof(int));
+            setsockopt(client_sock, IPPROTO_TCP, TCP_KEEPINTVL, &KEEP_INTERVAL, sizeof(int));
+            setsockopt(client_sock, IPPROTO_TCP, TCP_KEEPCNT, &KEEP_COUNT, sizeof(int));
 
             /** Convert ip address to string **/
             if (source_addr.ss_family == PF_INET) {
@@ -210,19 +217,19 @@ void app_controller(void* pvParameters) {
             }
             ESP_LOGI(TAG, "Socket accepted ip address: %s", s_addr_str);
 
-            interact(sock);
+            interact(client_sock); // TODO: Add multiprocess.
 
-            shutdown(sock, 0);
-            close(sock);
+            shutdown(client_sock, 0);
+            close(client_sock);
         }
 
 socket_error:
         device_delay_ms(100);
-        if (listen_sock != -1) {
+        if (server_sock != -1) {
             ESP_LOGE(TAG, "Shutting down socket and restarting...");
-            shutdown(listen_sock, 0);
+            shutdown(server_sock, 0);
             device_delay_ms(100);
-            close(listen_sock);
+            close(server_sock);
         }
     }
 
