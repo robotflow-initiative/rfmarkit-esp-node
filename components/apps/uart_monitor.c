a52ea2add9a499042f187f93f97f8f0b0e012a89
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2021-12-08 20:10:21 +0800

    Bug fix: add imu_data_trash back - to collect outdated value

--- components/apps/uart_monitor.c
+++ components/apps/uart_monitor.c
@@ -15,24 +15,19 @@
 #include "imu.h"
 #include "device.h"
 
-/** pseudo data **/
-#if CONFIG_USE_PSEUDO_VALUE
-char pseudo_data1[40] = { 0xA4, 0x03, 0x08, 0x12, 0x00, 0x07,
-                          0xFF, 0xFE, 0x08, 0x0A, 0xFF, 0xFE,
-                          0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                          0x00, 0x24, 0x00, 0x00, 0x00, 0x00,
-                          0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                          0x00, 0x04 };
-
-char pseudo_data2[40] = { 0xA4, 0x03, 0x08, 0x27, 0xFC, 0xFF,
-                          0x02, 0x00, 0x03, 0xF0, 0x01, 0x00,
-                          0xFF, 0xFF, 0x00, 0x00, 0x04, 0x00,
-                          0xFB, 0xFF, 0x18, 0x17, 0xED, 0xEC,
-                          0x0C, 0xe0, 0x04 ,0x03, 0x02, 0x0F,
-                          0xFB, 0xBE };
-#endif
-
 static const char* TAG = "app_uart_monitor";
+
+/**
+ * @brief Compute and set timestamp with us resolution
+ * 
+ */
+#define tag_time_us(time) \
+        { \
+                struct timeval tv_now = { 0 }; \
+                gettimeofday(&tv_now, NULL); \
+                time = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec; \
+        }
+
 
 void app_uart_monitor(void* pvParameters) {
     ESP_LOGI(TAG, "app_uart_monitor started");
@@ -43,6 +38,7 @@ void app_uart_monitor(void* pvParameters) {
     struct timeval tv_now = { 0 };
 
     imu_dgram_t imu_data = { 0 };
+    imu_dgram_t imu_data_trash = { 0 };
     int ret = 0;
 
     /** Wait until tcp connection is established, time synced and uart not blocked**/
@@ -79,27 +75,22 @@ void app_uart_monitor(void* pvParameters) {
         ESP_LOGD(TAG, "Try to read gy");
 
         /** Tag starting point */
-        gettimeofday(&tv_now, NULL);
-        imu_data.start_time_us =  (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
+        tag_time_us(imu_data.start_time_us);
 
         imu_read(&g_imu);
         memcpy(imu_data.data, g_imu.buf, sizeof(g_imu.buf));
 
         /** Tag stop point **/
-        gettimeofday(&tv_now, NULL);
+        tag_time_us(imu_data.time_us);
+        
         /** Count how many bits are left in the buffer **/
         size_t uart_buffer_len = 0;
         uart_get_buffered_data_len(g_imu.port, &uart_buffer_len);
-        imu_data.time_us = ((int64_t)tv_now.tv_sec - ((int64_t)uart_buffer_len / (CONFIG_IMU_DEFAULT_FREQ * CONFIG_IMU_PAYLOAD_LEN))) * 1000000L + (int64_t)tv_now.tv_usec;
-        
-        // imu_data.time_us = ((int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec);
-
         imu_data.uart_buffer_len = uart_buffer_len;
-
-#endif
+        
         /** If queue is full, clear queue **/
         while (uxQueueSpacesAvailable(serial_queue) <= 0) {
-            xQueueReceive(serial_queue, (void*)&imu_data, (TickType_t)0xF);
+            xQueueReceive(serial_queue, (void*)&imu_data_trash, (TickType_t)0xF);
             ESP_LOGE(TAG, "Buffer full\n");
         }
 
